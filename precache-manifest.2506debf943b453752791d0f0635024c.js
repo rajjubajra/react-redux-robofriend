bc3a9409ba64b1240874a9c91f907a3e04afb01e
rajju <vajrachara@yahoo.com>
2019-11-16 08:48:21 +0000

    Updates

---
+++ precache-manifest.2506debf943b453752791d0f0635024c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f60e84958e43ffb0ee6bad7f0fe303",
+    "url": "/react-redux-robofriend/index.html"
+  },
+  {
+    "revision": "db67ba71708ce8190706",
+    "url": "/react-redux-robofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f869286c105387c893d7",
+    "url": "/react-redux-robofriend/static/css/main.ddb45eab.chunk.css"
+  },
+  {
+    "revision": "db67ba71708ce8190706",
+    "url": "/react-redux-robofriend/static/js/2.d8323dc2.chunk.js"
+  },
+  {
+    "revision": "f869286c105387c893d7",
+    "url": "/react-redux-robofriend/static/js/main.6a65d9ed.chunk.js"
+  },
+  {
+    "revision": "d075fea793e10dcb64f2",
+    "url": "/react-redux-robofriend/static/js/runtime-main.63cd0849.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-redux-robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rajjubajra/react-redux-robofriend.git/precache-manifest.45c3f9856b357ef6c2e854ab1a20bc04.js"
+  "/react-redux-robofriend/precache-manifest.2506debf943b453752791d0f0635024c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rajjubajra/react-redux-robofriend.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-robofriend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
