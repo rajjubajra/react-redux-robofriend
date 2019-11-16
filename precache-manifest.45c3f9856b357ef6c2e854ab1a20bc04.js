c4d0466f000e9359c0f2b7ada2a00377811c8652
rajju <vajrachara@yahoo.com>
2019-11-16 08:38:56 +0000

    Updates

---
+++ precache-manifest.45c3f9856b357ef6c2e854ab1a20bc04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9178cf47d0ee9b4067d93e422e677a32",
+    "url": "/rajjubajra/react-redux-robofriend.git/index.html"
+  },
+  {
+    "revision": "db67ba71708ce8190706",
+    "url": "/rajjubajra/react-redux-robofriend.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d3da53ea0082c0ba1389",
+    "url": "/rajjubajra/react-redux-robofriend.git/static/css/main.0bfdb68b.chunk.css"
+  },
+  {
+    "revision": "db67ba71708ce8190706",
+    "url": "/rajjubajra/react-redux-robofriend.git/static/js/2.d8323dc2.chunk.js"
+  },
+  {
+    "revision": "d3da53ea0082c0ba1389",
+    "url": "/rajjubajra/react-redux-robofriend.git/static/js/main.2ac73997.chunk.js"
+  },
+  {
+    "revision": "68996dbd21c645d9980d",
+    "url": "/rajjubajra/react-redux-robofriend.git/static/js/runtime-main.ed37008e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/rajjubajra/react-redux-robofriend.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
