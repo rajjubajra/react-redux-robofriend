b0b1992e6efe0a6690b379d7b6de0bcd614124d9
rajju <vajrachara@yahoo.com>
2019-11-17 04:42:20 +0000

    robofriend with state reloaded

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,39 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
-
-import { setSearchField  } from '../action';
-
-
-const mapStateToProps = (state) => {
-  return{
-    searchField: state.searchField
-  }
-}
-
-const mapDispatchToProps = (dispatch) =>  {
-  return{
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-  }  
-}
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       robots: [],
+      searchfield: ''
     }
   }
 
   componentDidMount() {
-    //console.log(this.props.store.getState());
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
       .then(users => {this.setState({ robots: users})});
   }
 
-  
-  render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
 
+  render() {
+    const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !robots.length ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={ onSearchChange }/>
+          <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
@@ -57,4 +42,4 @@ class App extends Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
